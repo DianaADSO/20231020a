a74613a71f21b43ccd876f6d0ea41d8ff2d36e34
Diana rodríguez <128723615+DianaADSO@users.noreply.github.com>
2023-10-20 07:36:59 -0500

    versión 1.1

--- funciones.php
+++ funciones.php
@@ -1,5 +1,8 @@
 <?php
 
+/** Esta función se encarda de consultar todos los datos de la tabla  tb_usuarios
+ *  @Return     número     retorna el resultado de consultar los datos de la tabla
+ */
 //Función para consultar los datos de la tabla  tb_usuarios
 function consultar(){
 
@@ -7,8 +10,8 @@ function consultar(){
 
     //Conexión con la base de datos
     $connexion  = mysqli_connect('localhost', 'root', 'root', 'db_proyecto_ddm');  //Conexión con la base de datos
-    $sql        = "select * from tb_usuarios";
-    $resultado  = $connexion->query($sql);
+    $sql        = "select * from tb_usuarios"; //slecet para consultar los datos de todos los usuarios
+    $resultado  = $connexion->query($sql);  //Resultado de ejecutar el query
 
     //Recorre el recordset
     while( $fila = mysqli_fetch_assoc($resultado)){
@@ -20,10 +23,9 @@ function consultar(){
     }
 
     $connexion->close();//Cerrar conexion
-    return $salida; //Retorna la operación
+    return $salida; //Retorna el resultado
 
 }
-
 
 
 
